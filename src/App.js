f973314dbc7614a931c7febaa04760db670b8810
Ben West <benwestdev@gmail.com>
2020-10-08 10:04:00 -0400

    init repo

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const canvasRef = useRef(null);
+  const linkDownRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const [direction, setDirection] = useState("down");
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  // set the height and width of canvas
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    let theLinkRef;
+    if (direction === "down") theLinkRef = linkDownRef;
+    if (direction === "up") theLinkRef = linkUpRef;
+    if (direction === "left") theLinkRef = linkLeftRef;
+    if (direction === "right") theLinkRef = linkRightRef;
+
+    context.drawImage(theLinkRef.current, x, y);
+  }, [x, y]);
+
+  // set event listener for key actions
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    function handleKeyDown(e) {
+      if (e.key === "ArrowUp") move("up");
+      if (e.key === "ArrowLeft") move("left");
+      if (e.key === "ArrowDown") move("down");
+      if (e.key === "ArrowRight") move("right");
+    }
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  function move(dir) {
+    setDirection(dir);
+
+    if (direction === "up") setY((y) => y - 20);
+    if (direction === "down") setY((y) => y + 20);
+    if (direction === "left") setX((x) => x - 20);
+    if (direction === "right") setX((x) => x + 20);
+  }
+
+  return (
+    <div className="app">
+      <canvas ref={canvasRef} />
+
+      <div className="arrows">
+        <button onClick={() => move("up")}>Up</button>
+        <button onClick={() => move("left")}>Left</button>
+        <button onClick={() => move("down")}>Down</button>
+        <button onClick={() => move("right")}>Right</button>
+      </div>
+
+      <div className="images">
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
+      </div>
+    </div>
+  );
+}
